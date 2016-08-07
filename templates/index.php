ac31b9c54e28bbaa3c35d7b78077ba3250f5d7e7
Rello <Rello@users.noreply.github.com>
2016-08-07 13:29:59 +0200

    occ app:check findings

--- templates/index.php
+++ templates/index.php
@@ -12,7 +12,7 @@
 	script( 'audioplayer', 'app' );
 	
 ?>
-<form style="display:none;" class="float" id="file_upload_form" action="<?php print_unescaped(\OCP\Util::linkToRoute('audioplayer.photo.uploadPhoto')); ?>" method="post" enctype="multipart/form-data" target="file_upload_target">
+<form style="display:none;" class="float" id="file_upload_form" action="<?php print_unescaped(\OC::$server->getURLGenerator()->linkToRoute('audioplayer.photo.uploadPhoto')); ?>" method="post" enctype="multipart/form-data" target="file_upload_target">
 	<input type="hidden" name="id" value="">
 	<input type="hidden" name="requesttoken" value="<?php p($_['requesttoken']) ?>">
 	<input type="hidden" name="MAX_FILE_SIZE" value="<?php p($_['uploadMaxFilesize']) ?>" id="max_upload">
