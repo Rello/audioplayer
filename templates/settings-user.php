08b4c69d17b5b6654298a269394cb930bb24cbbb
Rello <Rello@users.noreply.github.com>
2016-08-27 21:55:01 +0200

    Add files via upload

--- templates/settings-user.php
+++ templates/settings-user.php
@@ -27,16 +27,5 @@
 		<input type="checkbox" id="cyrillic_user" <?php p($_['cyrillic']); ?>/>
 		<p><em><?php p($l->t('Activate this setting if cyrillic characters are not recognized correctly. This makes the indexing slower!')); ?></em></p>
 		<p><em><a href="https://github.com/Rello/audioplayer/wiki/Cyrillic-symbol-handling" target="_blank"><?php p($l->t('==> Details')); ?></a></em></p>
-	<!--<h3>&nbsp;</h3>
-		<label for="timer_user"><?php p($l->t('Test for user settings')); ?>:</label>
-		<input type="text" id="category" disabled value="<?php p($_['category']); ?>" />
-	<h3>&nbsp;</h3>
-	<h3><?php p($l->t('!!! BETA !!! Sleep Timer')); ?></h3>
-		<label for="timer_user"><?php p($l->t('User')); ?>:</label>
-		<input type="text" id="timer_user" value="kind" />
-		<label for="timer_time"><?php p($l->t('Time')); ?>:</label>
-		<input type="text" id="timer_time" value="7:25" />
-		<input type="submit" id="timer_button" value="<?php p($l->t('Save Timer')); ?>" />
-		<p><em><?php p($l->t('Select the Time (for the selected User) at when the Audioplayer will stop its playbacks')); ?></em></p>-->
 	</div>
 </div>
