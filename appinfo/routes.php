336f62568c1cacdefc29929bb4fb834c81acdac8
Rello <Rello@users.noreply.github.com>
2016-12-10 13:01:46 +0100

    getpublicaudioinfo

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -43,7 +43,7 @@ $application->registerRoutes($this, ['routes' => [
 	['name' => 'music#getAudioStream', 'url' => '/getaudiostream', 'verb' => 'GET'],
 	['name' => 'music#resetMediaLibrary', 'url' => '/resetmedialibrary', 'verb' => 'GET'],
 	['name' => 'music#getPublicAudioStream', 'url' => '/getpublicaudiostream{file}', 'verb' => 'GET'],
-	['name' => 'music#getPublicAudioInfo', 'url' => '/getpublicaudioinfo', 'verb' => 'GET'],
+	['name' => 'music#getPublicAudioInfo', 'url' => '/getpublicaudioinfo{file}', 'verb' => 'GET'],
 	['name' => 'photo#uploadPhoto',	'url' => '/uploadphoto',	'verb' => 'POST'],
 	['name' => 'photo#getImageFromCloud',	'url' => '/getimagefromcloud',	'verb' => 'GET'],
 	['name' => 'photo#cropPhoto',	'url' => '/cropphoto',	'verb' => 'POST'],
