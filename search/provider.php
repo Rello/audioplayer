8d64c96f83e8bf90a811c9fbc9e2d38969cc9453
Rello <Rello@users.noreply.github.com>
2017-03-21 06:31:37 +0100

    rework of search integration - provider
    #155

--- search/provider.php
+++ search/provider.php
@@ -19,18 +19,15 @@ use OCA\audioplayer\AppInfo\Application;
  */
 class Provider extends \OCP\Search\Provider {
 
-    #private $playlistController;
 	private $musicController;
-	
 	private $l10N;
 	
     public function __construct() {
 		$app = new Application();
 		$container = $app->getContainer();
 		$this->app = $app;
-		#$this->playlistController = $container->query('PlaylistController');
 		$this->musicController = $container->query(\OCA\audioplayer\Controller\MusicController::class);
-		$this->l10N = $container->query('L10N');
+		$this->l10n = $container->query('L10N');
 	}
 	
 	/**
@@ -46,15 +43,13 @@ class Provider extends \OCP\Search\Provider {
 		$searchresults = array();
 		$results = $this->musicController->searchProperties($query);
 		
-		
 		foreach($results as $result) {
-				
 			$returnData['id'] = $result['id'];
-			$returnData['description'] = $result['name'];
-			$returnData['link'] = '../audioplayer/#show-album-' . $result['id'];
+			$returnData['description'] = $this->l10n->t('Audio Player').' - '.$result['name'];
+			$returnData['link'] = '../audioplayer/#' . $result['id'];
 			$returnData['icon'] = '../audioplayer/img/app.svg';
-			
-		    $searchresults[] = new \OCA\audioplayer\Search\Result($returnData);
+
+			$searchresults[] = new \OCA\audioplayer\Search\Result($returnData);
 		}
 		return $searchresults;
 	}
