2e83e0b4ce472e414216400a3ff4fecd551bb5a9
Thomas Marx <xraMsamohT@users.noreply.github.com>
2017-04-17 21:26:52 +0200

    URLs in lowercase

--- js/app.js
+++ js/app.js
@@ -526,7 +526,7 @@ Audios.prototype.loadAlbums = function(){
 				$this.AlbumContainer.show();
 				$this.AlbumContainer.html('<span class="no-songs-found">'+t('audioplayer','Welcome to')+' '+t('audioplayer','Audio Player')+'</span>');
 				$this.AlbumContainer.append('<span class="no-songs-found-pl"><i class="ioc ioc-refresh" title="'+t('audioplayer','Scan for new audio files')+'" id="scanAudiosFirst"></i> '+t('audioplayer','Add new tracks to library')+'</span>');
-				$this.AlbumContainer.append('<a class="no-songs-found-pl" href="https://github.com/Rello/audioplayer/wiki" target="_blank">'+t('audioplayer','Help')+'</a>');
+				$this.AlbumContainer.append('<a class="no-songs-found-pl" href="https://github.com/rello/audioplayer/wiki" target="_blank">'+t('audioplayer','Help')+'</a>');
 				$('#app-navigation').removeClass('mp3_hide');
 			}			
 		}
