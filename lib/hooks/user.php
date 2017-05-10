8680f4c0ded96768a05724cea1d83d1b24167b49
Rello <Rello@users.noreply.github.com>
2017-05-10 13:48:15 +0200

    Hook for deleted user
    activate hook to cleanup data when user is deleted

---
+++ lib/hooks/user.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Audio Player
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the LICENSE.md file.
+ *
+ * @author Marcel Scherello <audioplayer@scherello.de>
+ * @copyright 2016-2017 Marcel Scherello
+ */
+ 
+namespace OCA\audioplayer\Hooks;
+use OCA\audioplayer\Controller;
+
+class User {
+    public static function deleteUser($params) {
+      $userId = $params['uid'];
+      $this->reset->resetMediaLibrary($userId);
+	}    
+}
