5962767b7c6ac3017f7255b1400898a655a91c2c
Thomas Marx <xraMsamohT@users.noreply.github.com>
2017-05-21 18:10:47 +0200

    New track notifier

--- l10n/de.js
+++ l10n/de.js
@@ -5,6 +5,7 @@ OC.L10N.register(
 	"Audio Player" : "Audio-Player",
 	"Add new tracks to library" : "Der Bibliothek neue Titel hinzufügen",
 	"Scan for new audio files" : "Nach neuen Audiodateien suchen",
+	"New audio files available" : "Neue Audiodateien verfügbar",
 	"Help" : "Hilfe",
 	"Sort playlist" : "Wiedergabeliste sortieren",
 	"Rename playlist" : "Wiedergabeliste umbenennen",
