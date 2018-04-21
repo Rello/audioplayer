60a2aa9b0c2a4e9d1c2b084b8bb90d0a51f278e1
Thomas Marx <xraMsamohT@users.noreply.github.com>
2018-04-21 13:04:15 +0200

    Data

--- l10n/ru.js
+++ l10n/ru.js
@@ -39,6 +39,8 @@ OC.L10N.register(
 	"No playlist entry" : "No playlist entry",
 	"Lyrics" : "Lyrics",
 	"No lyrics found" : "No lyrics found",
+	"Reading data" : "Reading data",
+	"No data" : "No data",
 	"Remove" : "Remove",
 	"Options" : "Options",
 	"MIME type" : "MIME type",
