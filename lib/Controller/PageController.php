51db53931e4e2af6d02a382e63365d094e31e294
Rello <github@scherello.de>
2019-01-27 13:21:47 +0100

    don´t show notification on clean installs

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -90,7 +90,7 @@ class PageController extends Controller
         #$app_version = $this->configManager->getAppValue($this->appName, 'installed_version', '0.0.0');
         $scanner_version = $this->configManager->getUserValue($this->userId, $this->appName, 'scanner_version', '0.0.0');
         //\OCP\Util::writeLog('audioplayer', 'scanner version: '.$scanner_version, \OCP\Util::DEBUG);
-        if (version_compare($scanner_version, '2.3.0', '<')) {
+        if (version_compare($scanner_version, '2.3.0', '<') AND $scanner_version !== '0.0.0') {
             return '<a href="https://github.com/rello/audioplayer/blob/master/CHANGELOG.md">' . $this->l10n->t('Please reset and rescan library to make use of new features.') . ' ' . $this->l10n->t('More information…') . '</a>';
         } else {
             return null;
